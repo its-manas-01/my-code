7fd35c2416ae996efa4acfb44d5c58115549b8a2
its-manas-01 <ghoshmanas309@gmail.com>
2024-05-21 07:04:37 +0530

    java

--- Java/e.java
+++ Java/e.java
@@ -1,8 +1,19 @@
+// public class e {
+//     public static void main(String[] args) {
+//         int a=Integer.parseInt(args[0]);
+//         int b=Integer.parseInt(args[1]);
+//         int c=a+b;
+//         System.out.println(c);
+//     }
+// }
 public class e {
     public static void main(String[] args) {
         int a=Integer.parseInt(args[0]);
         int b=Integer.parseInt(args[1]);
-        int c=a+b;
-        System.out.println(c);
+        System.out.println("value before swap are:-"+a+"&"+b);
+        a+=b;
+        b=a-b;
+        a-=b;
+        System.out.println("value after swap are:-"+a+"&"+b);
     }
 }
