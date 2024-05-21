3d9164e04ce8124e9eafa46616afbf39e0ab8715
its-manas-01 <ghoshmanas309@gmail.com>
2024-05-21 06:56:14 +0530

    java

--- Java/e.java
+++ Java/e.java
@@ -1,5 +1,8 @@
 public class e {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        int c=a+b;
+        System.out.println(c);
     }
 }
