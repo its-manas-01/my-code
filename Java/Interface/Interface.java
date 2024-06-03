d832c5fa33fb1cfe6899bb081da1fc321a958bc2
its-manas-01 <ghoshmanas309@gmail.com>
2024-06-03 20:51:32 +0530

    java

---
+++ Java/Interface/Interface.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+/**
+ * InnerInterface
+ */
+interface  x{
+    static final int a=5;
+    void cal(int b);   
+}
+class m implements x{
+    int c,d;
+    public void cal(int b)
+    {
+        c=b;
+    }
+    void calculate()
+    {
+        d=(a+c);
+        System.out.println(d);
+    }
+}
+public class Interface {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int c=scanner.nextInt();
+        m ob =new m();
+        ob.cal(c);
+        ob.calculate();
+        scanner.close();
+    }
+}
